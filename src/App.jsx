19d78714b6e0df7a3930fa75053f890e00ef02c4
Safia Messaoud <81869764+safiames@users.noreply.github.com>
2023-12-11 14:30:13 +0200

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
 import React, { useState } from "react";
 
+const PersonForm = ({
+  addPerson,
+  newName,
+  newNumber,
+  handleNameChange,
+  handleNumberChange,
+}) => (
+  <form onSubmit={addPerson}>
+    <div>
+      name: <input value={newName} onChange={handleNameChange} />
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+);
+
+const PersonList = ({ persons }) => (
+  <div>
+    <h2>Numbers</h2>
+    <ul>
+      {persons.map((person, index) => (
+        <li key={index}>
+          {person.name} - {person.number}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "123-456-7890" },
@@ -37,25 +70,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input onChange={handleNameChange} value={newName} />
-        </div>
-        <div>
-          number: <input onChange={handleNumberChange} value={newNumber} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map((person, index) => (
-          <li key={index}>
-            {person.name} - {person.number}
-          </li>
-        ))}
-      </ul>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
+      <PersonList persons={persons} />
     </div>
   );
 };
